2a45c0bae4f29b0414f200f1d10c5e801f650aa7
Steven Wyman <89478653+Knighty2501@users.noreply.github.com>
2021-08-30 11:09:34 -0600

    Helllloooo World!

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    //man git is weirding me out with the PAT thing I had an odd time getting that to work for some reason lol... 
+    printf("Hello World!\n");
+
+    return 0;
+    }
